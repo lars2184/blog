4d1fa44d9ff7bd3255cc136567b2cb292db65c5e
Lars Jacobson <larsjacobson@gmail.com>
2015-05-11 10:23:44 -0700

    update glossary

--- js/glossary.js
+++ js/glossary.js
@@ -10,7 +10,13 @@ function initGlossary(){
   $glossaryList.html(" ");
   $.each(glossaryDictionary,function(index, entry){
 
-    $glossaryList.append("<dt>" + entry.word + "</dt><dd>" + entry.definition + "</dd>");
+    if(entry.url != ""){
+
+      $glossaryList.append("<dt><a href='"+entry.url+"' target='blank'>" + entry.word + "</a></dt><dd>" + entry.definition + "</dd>");
+    }else{
+      $glossaryList.append("<dt>" + entry.word + "</dt><dd>" + entry.definition + "</dd>");
+    }
+    
   });
 
 }
@@ -18,11 +24,13 @@ function initGlossary(){
 function initGlossaryDictionary(){
 
   var dictionary = [
-    {word: "lorem", definition:"something greek"},
-    {word: "ipsum", definition:"something else greek"},
-    {word: "ipsum", definition:"something else greek"},
-    {word: "ipsum", definition:"something else greek"},
-    {word: "ipsum", definition:"something else greek"}
+  {word: "$.on()", definition:"Attach an event handler function for one or more events to the selected elements.",url:"http://api.jquery.com/on/"},
+  {word: "$.html()", definition:"Get the HTML contents of the first element in the set of matched elements or set the HTML contents of every matched element.",url:"https://api.jquery.com/html/"},
+  {word: "$.text()", definition:"Get the combined text contents of each element in the set of matched elements, including their descendants, or set the text contents of the matched elements.",url:"http://api.jquery.com/text/"},
+    {word: "$.prepend()", definition:"Insert content, specified by the parameter, to the beginning of each element in the set of matched elements.",url:"http://api.jquery.com/prepend/"},
+    {word: "$.append()", definition:"Insert content, specified by the parameter, to the end of each element in the set of matched elements.",url:"http://api.jquery.com/append/"},
+    {word: "$.after()", definition:"Insert content, specified by the parameter, after each element in the set of matched elements.",url:"http://api.jquery.com/after/"},
+    {word: "$.before()", definition:"Insert content, specified by the parameter, before each element in the set of matched elements.",url:"http://api.jquery.com/before/"}
   ];
 
   return(dictionary);
--- js/home.js
+++ js/home.js
@@ -11,7 +11,12 @@ function initHomeGlossary(){
   $.each(glossaryDictionary,function(index, entry){
 
     if(index < 2){
+      if(entry.url != ""){
+
+      $glossaryList.append("<dt><a href='"+entry.url+"' target='blank'>" + entry.word + "</a></dt><dd>" + entry.definition + "</dd>");
+    }else{
       $glossaryList.append("<dt>" + entry.word + "</dt><dd>" + entry.definition + "</dd>");
+    }
     }
   });
 }
