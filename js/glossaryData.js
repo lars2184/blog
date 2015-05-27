9db3cfbf20d0624ba71b1510632a9a93ce5ed7a5
Lars Jacobson <larsjacobson@gmail.com>
2015-05-26 17:05:57 -0700

    add project management

--- js/glossaryData.js
+++ js/glossaryData.js
@@ -1,6 +1,6 @@
 {
     var dictionary = [
-      {word: "$.on()", definition:"Attach an event handler function for one or more events to the selected elements.",url:"http://api.jquery.com/on/"},
+      {word: "$.on()", definition:"Attach11111 an event handler function for one or more events to the selected elements.",url:"http://api.jquery.com/on/"},
       {word: "$.html()", definition:"Get the HTML contents of the first element in the set of matched elements or set the HTML contents of every matched element.",url:"https://api.jquery.com/html/"},
       {word: "$.text()", definition:"Get the combined text contents of each element in the set of matched elements, including their descendants, or set the text contents of the matched elements.",url:"http://api.jquery.com/text/"},
       {word: "$.prepend()", definition:"Insert content, specified by the parameter, to the beginning of each element in the set of matched elements.",url:"http://api.jquery.com/prepend/"},
