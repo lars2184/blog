55de8e6e210faa3ffe8b4029cdcebebfcfd32e58
Lars Jacobson <larsjacobson@gmail.com>
2015-05-26 17:26:08 -0700

    load json from file

--- js/glossary.js
+++ js/glossary.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 });
 
 var xhr = new XMLHttpRequest();
+var glossaryFromFile;
 
 function initGlossary(){
 
@@ -17,7 +18,14 @@ function initGlossary(){
 
   xhr.onreadystatechange = function () {
      if (xhr.readyState === 4) {
-       console.log("result = "+xhr.responseText)
+       console.log("result = "+xhr.responseText);
+
+       glossaryFromFile = JSON.parse(xhr.responseText);
+       //glossaryFromFile = xhr.responseText;
+
+       console.log("glossaryFromFile.dictionary = " + glossaryFromFile.dictionary);
+
+       //glossaryFromFile.dictionary
      }
   };
   
@@ -96,15 +104,19 @@ function getGlossary(){
 
 function initGlossaryDictionary(){
 
-  var dictionary = [
-  {word: "$.on()", definition:"Attach an event handler function for one or more events to the selected elements.",url:"http://api.jquery.com/on/"},
-  {word: "$.html()", definition:"Get the HTML contents of the first element in the set of matched elements or set the HTML contents of every matched element.",url:"https://api.jquery.com/html/"},
-  {word: "$.text()", definition:"Get the combined text contents of each element in the set of matched elements, including their descendants, or set the text contents of the matched elements.",url:"http://api.jquery.com/text/"},
-    {word: "$.prepend()", definition:"Insert content, specified by the parameter, to the beginning of each element in the set of matched elements.",url:"http://api.jquery.com/prepend/"},
-    {word: "$.append()", definition:"Insert content, specified by the parameter, to the end of each element in the set of matched elements.",url:"http://api.jquery.com/append/"},
-    {word: "$.after()", definition:"Insert content, specified by the parameter, after each element in the set of matched elements.",url:"http://api.jquery.com/after/"},
-    {word: "$.before()", definition:"Insert content, specified by the parameter, before each element in the set of matched elements.",url:"http://api.jquery.com/before/"}
-  ];
+  console.log("initGlossaryDictionary");
+
+  var dictionary = glossaryFromFile.dictionary;
+
+  // var dictionary = [
+  // {word: "$.on()", definition:"Attach an event handler function for one or more events to the selected elements.",url:"http://api.jquery.com/on/"},
+  // {word: "$.html()", definition:"Get the HTML contents of the first element in the set of matched elements or set the HTML contents of every matched element.",url:"https://api.jquery.com/html/"},
+  // {word: "$.text()", definition:"Get the combined text contents of each element in the set of matched elements, including their descendants, or set the text contents of the matched elements.",url:"http://api.jquery.com/text/"},
+  //   {word: "$.prepend()", definition:"Insert content, specified by the parameter, to the beginning of each element in the set of matched elements.",url:"http://api.jquery.com/prepend/"},
+  //   {word: "$.append()", definition:"Insert content, specified by the parameter, to the end of each element in the set of matched elements.",url:"http://api.jquery.com/append/"},
+  //   {word: "$.after()", definition:"Insert content, specified by the parameter, after each element in the set of matched elements.",url:"http://api.jquery.com/after/"},
+  //   {word: "$.before()", definition:"Insert content, specified by the parameter, before each element in the set of matched elements.",url:"http://api.jquery.com/before/"}
+  // ];
 
   return(dictionary);
 }
